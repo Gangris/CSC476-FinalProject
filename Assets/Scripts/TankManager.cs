cfa54925bbc06193df5515dff1486b00d0e79b36
Adarian Eastman <adarianeastman@gmail.com>
2018-11-28 17:41:19 -0500

    Added turret that points towards cursor

--- Assets/Scripts/TankManager.cs
+++ Assets/Scripts/TankManager.cs
@@ -6,42 +6,59 @@ public class TankManager : MonoBehaviour
 {
     public Rigidbody rb;
     public GameObject tankBody;
+    public GameObject tankTurret;
     public int baseMoveMultiplyer = 4;
     public int upgradeMoveMultiplyer = 0;
 
 	void Start ()
 	{
 	    rb = GetComponent<Rigidbody>();
+
 	    Transform tbtrans = gameObject.transform.Find("Body");
 	    tankBody = tbtrans.gameObject;
+
+	    Transform tttrans = gameObject.transform.Find("Turret");
+	    tankTurret = tttrans.gameObject;
 	}
 	
 	void Update () {
 	    MovementInput();
-    }
+	    UpdateTurretPosition();
+	}
 
     void MovementInput()
     {
         if (Input.GetKey(KeyCode.W))
         {
+            // Add multiplayer method for server to handle movement
             rb.AddForce(tankBody.transform.up * (baseMoveMultiplyer + upgradeMoveMultiplyer));
         }
 
         if (Input.GetKey(KeyCode.S))
         {
+            // Add multiplayer method for server to handle movement
             rb.AddForce(-tankBody.transform.up * (baseMoveMultiplyer + upgradeMoveMultiplyer));
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            //rb.AddForce(Vector3.left);
+            // Add multiplayer method for server to handle rotation
             tankBody.transform.Rotate(0, 0, 1f);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            //rb.AddForce(Vector3.right);
+            // Add multiplayer method for server to handle rotation
             tankBody.transform.Rotate(0, 0, -1f);
         }
+    }
+
+    void UpdateTurretPosition()
+    {
+        Vector3 mouse = Input.mousePosition;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        Vector2 direction = new Vector2(mouse.x - transform.position.x, mouse.y - transform.position.y);
+
+        tankTurret.transform.up = direction;
     }
 }
