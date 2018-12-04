cc301db99bb2eeb4fc320cb21167c067b9c7068c
William Parciak <why893@gmail.com>
2018-12-04 13:09:36 -0500

    added tower tracking enemy positions, and shooting them if they get into range

--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class TurretManager : DestructablePewPewTankObject
 {
+    GameObject[] enemies;
+    public GameObject round;
+    public GameObject turret;
+
+    public string teamName;
+    public int turretDamage = 100;
+
+    float timer = 0;
+    int waitingTime = 1;
+
     void Start()
     {
         Bootstrap();
         this.maxHealth = 1000;
         this.health = 1000;
+        if (this.team == Team.team1)
+            teamName = "team1";
+        else
+            teamName = "team2";
+    }
+
+    private void Update()
+    {
+            enemies = GameObject.FindGameObjectsWithTag(teamName);
+        foreach (GameObject target in enemies)
+        {
+            float distance = Vector3.Distance(target.transform.position, transform.position);
+            Vector2 targetPos = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
+
+            timer += Time.deltaTime;
+            if (distance < 2 && timer > waitingTime)
+            {
+                ShootTurret(targetPos);
+                timer = 0;
+            }
+        }
+
+    }
+
+    void ShootTurret(Vector2 direction)
+    {
+        GameObject newRound = Instantiate(round, turret.transform.position, Quaternion.identity);
+        RoundManager nrManager = newRound.GetComponent<RoundManager>();
+        nrManager.velocity = 4;
+        nrManager.direction = Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x) - 90f;
+        nrManager.owner = gameObject;
+        nrManager.team = team;
+        nrManager.damage = turretDamage;
+        
+
+        StartCoroutine(DelayedDestroyRound(newRound));
+    }
+
+    IEnumerator DelayedDestroyRound(GameObject newRound)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(newRound);
     }
 }
