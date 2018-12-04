d6b97f1dd2e6eff835cb657e8104fe2088441b8d
Andrew <ab850649@wcupa.edu>
2018-12-04 13:25:52 -0500

    Added Creep spawn waves. Still needs targets when instantiating and for Creep Objects to be prefabs in CreepManager Script.

--- Assets/Scripts/CreepManager.cs
+++ Assets/Scripts/CreepManager.cs
@@ -9,5 +9,29 @@ public class CreepManager : DestructablePewPewTankObject
         Bootstrap();
         this.health = 400;
         this.maxHealth = 400;
+        Spawn();
+        //calling Spawn function every spawnTime seconds.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    public GameObject Team1Creep;
+    public GameObject Team2Creep;
+    public GameObject T1midSpawn;
+    public GameObject T1botSpawn;
+    public GameObject T1topSpawn;
+    public GameObject T2midSpawn;
+    public GameObject T2botSpawn;
+    public GameObject T2topSpawn;
+    public float spawnTime = 45f;
+
+    void Spawn()
+    {
+        //creating a creep at each spawn point every time the Spawn function is called.
+        Instantiate(Team1Creep, T1topSpawn.transform.position, Quaternion.identity);
+        Instantiate(Team1Creep, T1midSpawn.transform.position, Quaternion.identity);
+        Instantiate(Team1Creep, T1botSpawn.transform.position, Quaternion.identity);
+        Instantiate(Team2Creep, T2topSpawn.transform.position, Quaternion.identity);
+        Instantiate(Team2Creep, T2midSpawn.transform.position, Quaternion.identity);
+        Instantiate(Team2Creep, T2botSpawn.transform.position, Quaternion.identity);
     }
 }
